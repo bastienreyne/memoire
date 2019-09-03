4462bd71abd4c5998b3be23f7208b7a199e0cb7e
bastienreyne <bastien.reyne@orange.fr>
2019-09-03 12:17:54 +0200

    ajout diapo

--- r/plot_larves_bloc2.R
+++ r/plot_larves_bloc2.R
@@ -20,4 +20,28 @@ data %>% ggplot +
     facet_grid(. ~ sol) +
     xlab(element_blank()) +
     ylab("Nombre de larves s'éjectant des inflorescences")
-                
+
+
+# Diapo -------------------------------------------------------------------
+
+library(tidyverse)
+data <- read_csv("/home/bastien/cecidomyie/data/2017_piege_bloc2.csv")
+load("/home/bastien/cecidomyie/data/date2017.Rdata")
+
+data$sol <- factor(rep(c("Enherbement ras",
+                         "Paillage synthétique", 
+                         "Enherbement haut"), each = 80),
+                   levels = c("Enherbement ras",
+                              "Paillage synthétique",
+                              "Enherbement haut"))
+
+data %>% ggplot +
+    aes(x = date, y = larves) +
+    theme_bw() +
+    geom_line(aes(y = inflos), col = "grey50", lwd = 0.75) +
+    geom_point() +
+    geom_line(linetype = "dotted") +
+    facet_grid(. ~ sol) +
+    xlab(element_blank()) +
+    ylab("Nombre de larves s'éjectant des inflorescences")
+
